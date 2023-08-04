4226f3524a450e2eaeec49026d93e162607356e6
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-08-04 18:59:25 +0100

    using a minimal web api approach, added a web api using an in memory database. Passed Get, Put, Delete and Post tests using postman. Added Run() and GetResourceto CrudService.cs

--- HTTPClientExercise/CrudService.cs
+++ HTTPClientExercise/CrudService.cs
@@ -12,7 +12,17 @@ namespace HTTPClientExercise
 
         public CrudService() 
         {
-            MyhtttpClient.BaseAddress = new Uri("http://localhost:5000");
+            MyhtttpClient.BaseAddress = new Uri("http://localhost:5045");
+            MyhtttpClient.Timeout = new TimeSpan(0, 0, 30);
+        }
+        public async Task Run()
+        {
+            await GetResource();
+        }
+        public async Task GetResource()
+        {
+            var response = await MyhtttpClient.GetAsync("api/todoitems");
+            response.EnsureSuccessStatusCode();
         }
     }
 }
